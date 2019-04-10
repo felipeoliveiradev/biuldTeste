b67c72e0cbea00db026b0192159cbafa4a59fb04
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2019-04-10 01:55:06 -0300

    commit inicial

---
+++ precache-manifest.dd36d43031eeecf554162d96a48b4516.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "11fc62ebefea896b9215",
+    "url": "/static/js/main.59876487.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73220d33a6a58f434b59",
+    "url": "/static/css/2.fa78b3d0.chunk.css"
+  },
+  {
+    "revision": "73220d33a6a58f434b59",
+    "url": "/static/js/2.a32f56d0.chunk.js"
+  },
+  {
+    "revision": "3f6e21d5b7262e386bc99db029dca18c",
+    "url": "/static/media/add_playlist.3f6e21d5.svg"
+  },
+  {
+    "revision": "3fd45499efe15cfa5ac26000494ec532",
+    "url": "/static/media/loading.3fd45499.svg"
+  },
+  {
+    "revision": "6b27f5927a792a595f190dda190747d6",
+    "url": "/static/media/volume.6b27f592.svg"
+  },
+  {
+    "revision": "54850ef47ae6d4d038552c89fafeb57b",
+    "url": "/static/media/shuffle.54850ef4.svg"
+  },
+  {
+    "revision": "198550c154cb26037ac4ec6b3a1fd664",
+    "url": "/static/media/backward.198550c1.svg"
+  },
+  {
+    "revision": "6557e51e1e6d913c5a89e62b2791cdc2",
+    "url": "/static/media/play.6557e51e.svg"
+  },
+  {
+    "revision": "ee8bd067458e4da29de46501efe95dee",
+    "url": "/static/media/pause.ee8bd067.svg"
+  },
+  {
+    "revision": "63fca2afee55e0fcfd8975b787a3908f",
+    "url": "/static/media/forward.63fca2af.svg"
+  },
+  {
+    "revision": "716dfa0b41f0008fced2daef09f22aeb",
+    "url": "/static/media/repeat.716dfa0b.svg"
+  },
+  {
+    "revision": "75bed367be0218560786fa10bb6f37e4",
+    "url": "/static/media/search.75bed367.svg"
+  },
+  {
+    "revision": "fc42ea00c262e7b6c84cf3dfa18bb297",
+    "url": "/static/media/close.fc42ea00.svg"
+  },
+  {
+    "revision": "52df934abf9666bb18f978501f03322f",
+    "url": "/static/media/clock.52df934a.svg"
+  },
+  {
+    "revision": "38b523e46c97e55c30ac3169007b0d4a",
+    "url": "/static/media/plus.38b523e4.svg"
+  },
+  {
+    "revision": "961ba0fa785633f3c937abd73b80a84e",
+    "url": "/static/media/cover.961ba0fa.svg"
+  },
+  {
+    "revision": "64e483c95a4f6bb01c2b3eda424d15c7",
+    "url": "/index.html"
+  }
+];
